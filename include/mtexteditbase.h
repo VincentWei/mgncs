a60f6a282816b8ce412028ce56e76c8331094ee6
VincentWei <vincent@minigui.org>
2018-02-01 18:41:57 +0800

    add copying info in alll source files

--- include/mtexteditbase.h
+++ include/mtexteditbase.h
@@ -1,12 +1,43 @@
-/*
-** $Id: mtexteditbase.h 13589 2010-10-29 08:50:13Z wanzheng $
-**
-** texteditbase.h: base header for the new text edit module.
-**
-** Copyright (C) 2010 Feynman Software.
-**
-** Create date: 2010/03/10
-*/
+/**
+ * \file texteditbase.h
+ * \author 
+ * \date 
+ *
+ * \brief base header for the new text edit module.
+ *
+ \verbatim
+
+    This file is part of mGNCS, a component for MiniGUI.
+
+    Copyright (C) 2008~2018, Beijing FMSoft Technologies Co., Ltd.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Or,
+
+    As this program is a library, any link to this program must follow
+    GNU General Public License version 3 (GPLv3). If you cannot accept
+    GPLv3, you need to be licensed from FMSoft.
+
+    If you have got a commercial license of this program, please use it
+    under the terms and conditions of the commercial license.
+
+    For more information about the commercial license, please refer to
+    <http://www.minigui.com/en/about/licensing-policy/>.
+
+ \endverbatim
+ */
 
 #ifndef TEXTEDITBASE_H
 #define TEXTEDITBASE_H
