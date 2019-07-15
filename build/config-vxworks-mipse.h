44ef08f165170ede2ba519fe76c7b2e512d79191
Vincent Wei <vincent@minigui.org>
2019-07-15 14:27:37 +0800

    remove Exception List Statement

--- build/config-vxworks-mipse.h
+++ build/config-vxworks-mipse.h
@@ -1,15 +1,3 @@
-///////////////////////////////////////////////////////////////////////////////
-//
-//                          IMPORTANT NOTICE
-//
-// The following open source license statement does not apply to any
-// entity in the Exception List published by FMSoft.
-//
-// For more information, please visit:
-//
-// https://www.fmsoft.cn/exception-list
-//
-//////////////////////////////////////////////////////////////////////////////
 /* mgncsconfig.h.  Generated from mgncsconfig.h.in by configure.  */
 /* mgncsconfig.h.in.  Generated from configure.in by autoheader.  */
 
