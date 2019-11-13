0736a2a70bde645e72f6e2ce42275483dedc98f8
Vincent Wei <vincent@minigui.org>
2019-11-13 11:56:07 +0800

    change link to licensing policy

--- include/pieces/marrowbuttonpiece.h
+++ include/pieces/marrowbuttonpiece.h
@@ -38,7 +38,7 @@
  *   under the terms and conditions of the commercial license.
  * 
  *   For more information about the commercial license, please refer to
- *   <http://www.minigui.com/en/about/licensing-policy/>.
+ *   <http://www.minigui.com/blog/minigui-licensing-policy/>.
  */
 #if defined (_MGNCSCTRL_IMWORDSEL) || defined (_MGNCSCTRL_SPINNER) || defined (_MGNCSCTRL_SCROLLBAR)
 
